97646a377496dabe5aa6048ce8e57e6bccb9631c
lipan <alp484@163.com>
2020-03-27 16:49:33 +0800

    dddd

---
+++ src/main/java/com/example/newspringcms/NewspringcmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.newspringcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspringcmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewspringcmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newspringcms/NewspringcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newspringcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspringcmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
