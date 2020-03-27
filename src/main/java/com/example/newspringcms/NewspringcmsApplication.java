bc6a826e4549fd183a9144309345ff0be4f6e465
lipan <alp484@163.com>
2020-03-27 17:01:22 +0800

    试试提交2 可以了

--- src/main/java/com/example/newspringcms/NewspringcmsApplication.java
+++ src/main/java/com/example/newspringcms/NewspringcmsApplication.java
@@ -9,6 +9,7 @@ public class NewspringcmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(NewspringcmsApplication.class, args);
         ///测试提交代码
+        System.out.println("哈哈哈 真的可以了");
     }
 
 }
